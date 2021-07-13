f009c05915ee1cc742c70e1ee6b057ede1db8f9b
Arav-J <aravjoshi2003@outlook.com>
2021-07-13 14:08:12 +0100

    replaced the logic statement and renamed a variable

--- Python_Files/Sorting.py
+++ Python_Files/Sorting.py
@@ -1,21 +1,24 @@
 # some_list = [3, 1, 4, 8]
 # [1, 3, 4, 8]
-
+"""
+Taking a list of numbers and organising them in order of size.
+First iteration took a list of 4 and and worked, but when the length of the list was increased
+there was an issue with the logic statements.
+The logic statements have now been adjusted.
+"""
 some_list = [3, 1, 4, 8, 2, 7, 5, 2, 3, 7, 8, 12]
-print("The list is : " + str(some_list))
-counter = 0
-for i in some_list:
-    counter = counter + 1
-print("Length of list is : " + str(counter))
 
-# def sort_list(some_list):
-#     for i in range(0, len(list)):
-#         if some_list[i] > some_list[i + 1]:
-#             new_number = some_list[i]
-#             some_list[i] = some_list[i + 1]
-#             some_list[i + 1] = new_number
-#
-#     return print(some_list)
-#
-#
-# sort_list()
+
+def sort_list(something):
+    i = 0
+    for i in range(i, len(something)):
+        for j in range(i + 1, len(something)):
+            if something[i] > something[j]:
+                new_number = something[i]
+                something[i] = something[j]
+                something[j] = new_number
+
+    return print(something)
+
+
+sort_list(some_list)
